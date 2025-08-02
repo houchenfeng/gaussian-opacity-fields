ddfc8bf325c38cd32a00f345c30e12ecb2c6f589
litian <18374328@buaa.edu.cn>
2025-08-02 19:45:11 +0800

    20250802

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0 python3 train.py -s /home/lt/2024/data/dtu_datasets/dtu_r
     
 # marching tetrahedra with binary search
 # cmd = f"OMP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES={gpu} python extract_mesh.py -m {output_dir}/scan{scene} --iteration 30000"
-CUDA_VISIBLE_DEVICES=0 python3 extract_mesh.py -m output/dtu_robust/scan24/data_0.3 --iteration 30000
+CUDA_VISIBLE_DEVICES=0 python3 extract_mesh_tsdf.py -m output/dtu_robust/scan24/data_0.3 --iteration 30000
 
 # # tsdf fusion
 # cmd = f"OMP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES={gpu} python extract_mesh_tsdf.py -m {output_dir}/scan{scene} --iteration 30000"
